94f1ad45846a00aed14285dd7183de95549dfef6
Zakora <raflisilehu20@gmail.com>
2026-07-22 14:36:41 +0700

    fix: fix install.sh script

--- install.sh
+++ install.sh
@@ -12,22 +12,21 @@ echo "Current user      : $CURRENT_USER"
 sudo sed \
     -e "s|__PROJECT_DIR__|$PROJECT_DIR|g" \
     -e "s|__USER__|$CURRENT_USER|g" \
-    systemd/geth-demo.service.template \
+    systemd/geth-demo.service \
     | sudo tee /etc/systemd/system/geth-demo.service > /dev/null
 
 sudo sed \
     -e "s|__PROJECT_DIR__|$PROJECT_DIR|g" \
     -e "s|__USER__|$CURRENT_USER|g" \
-    systemd/geth-alert@.service.template \
+    systemd/geth-alert@.service \
     | sudo tee /etc/systemd/system/geth-alert@.service > /dev/null
 
 sudo systemctl daemon-reload
 
 echo
-echo "Systemd services installed successfully."
-
+echo "Installation completed successfully."
 echo
 echo "Next steps:"
-echo "1. sudo systemctl enable geth-demo.service"
-echo "2. sudo systemctl restart geth-demo.service"
-echo "3. sudo systemctl status geth-demo.service"
+echo "sudo systemctl enable geth-demo.service"
+echo "sudo systemctl restart geth-demo.service"
+echo "sudo systemctl status geth-demo.service"
